3a1bf14e2548309d5598185c83ad508b5a65498d
Premnathsasi <premnathsasi4@gmail.com>
2023-07-27 13:05:47 +0530

    clean up code

--- Backend_Development/app.js
+++ Backend_Development/app.js
@@ -27,56 +27,9 @@
 // server.listen(4000);
 
 const http = require("http");
-const fs = require("fs");
-let textin = "";
 
-fs.readFile("./message.txt", "utf-8", (err, data) => {
-  if (!err) {
-    textin = data;
-  } else {
-    console.log(err);
-  }
-});
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(`<h1>${textin}</h1>`);
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const msg = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", msg, (err) => {
-        if (!err) {
-          fs.readFile("./message.txt", "utf-8", (err, data) => {
-            if (!err) {
-              textin = data;
-              res.statusCode = 302;
-              res.setHeader("Location", "/");
-              return res.end();
-            }
-          });
-        }
-      });
-    });
-  }
-  res.write("<html>");
-  res.write("<head><title> My first page</title></head>");
-  res.write("<body><h1>Welcome to my Node Js project </h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require('./routes')
+
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
