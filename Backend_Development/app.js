1cef2a3dfdebfdbf6db6d117f5b727d8f41c883f
Premnathsasi <premnathsasi4@gmail.com>
2023-08-01 12:14:22 +0530

    Lets get started with Express

--- Backend_Development/app.js
+++ Backend_Development/app.js
@@ -1,35 +1,16 @@
-// const http = require("http");
-// const name = "Premnath";
-// const server = http.createServer((req, res) => {
-//   const url = req.url;
-//   if (url === "/home") {
-//     res.write("<html>");
-//     res.write("<head><title> My first page</title></head>");
-//     res.write("<body><h1>Welcome home</h1></body>");
-//     res.write("</html>");
-//     return res.end();
-//   }
-//   if (url === "/about") {
-//     res.write("<html>");
-//     res.write("<head><title> My first page</title></head>");
-//     res.write("<body> <h1>Welcome to About Us page</h1></body>");
-//     res.write("</html>");
-//     return res.end();
-//   }
-//   res.setHeader("Content-Type", "text/html");
-//   res.write("<html>");
-//   res.write("<head><title> My first page</title></head>");
-//   res.write("<body><h1>Welcome to my Node Js project </h1></body>");
-//   res.write("</html>");
-//   res.end();
-// });
-
-// server.listen(4000);
-
 const http = require("http");
 
-const routes = require('./routes')
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
+app.use((req, res, next) => {
+    console.log('In the Middleware');
+    next()
+});
 
-server.listen(4000);
+app.use((req, res, next) => {
+    console.log('In another Middleware');
+    res.send('<h1>Hello from Express JS</h1>')
+});
+
+app.listen(4000)
