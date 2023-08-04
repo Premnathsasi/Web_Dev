a58f04f62bb37d866f0068e784fba2121d0871ac
Premnathsasi <premnathsasi4@gmail.com>
2023-08-04 14:27:44 +0530

    Serving HTML , CSS and JS (VIEWS)

--- Backend_Development/app.js
+++ Backend_Development/app.js
@@ -1,20 +1,24 @@
-const http = require("http");
-const bodyParser = require('body-parser');
+const path = require('path')
 
 const adminRoutes = require('./routes/admin');
-const shopRouter = require('./routes/shop')
+const shopRouter = require('./routes/shop');
+const contactRouter = require('./routes/contact')
+const successRouter = require('./routes/success')
 
 const express = require('express');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRouter);
+app.use(contactRouter);
+app.use(successRouter)
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 
---
+++ Backend_Development/routes/contact.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const express = require('express')
+
+const rootDir = require('../util/path')
+const router = express.Router();
+
+router.get('/contact', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contact.html'))
+})
+
+router.post('/contact', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/sucess')
+});
+
+module.exports = router
